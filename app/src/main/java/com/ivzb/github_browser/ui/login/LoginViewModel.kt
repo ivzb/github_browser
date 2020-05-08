48ba15521902f365802e3d17dba6dde3019daf2b
ivzb <izahariev96@gmail.com>
2020-05-08 17:23:56 +0300

    add login button to layout

--- app/src/main/java/com/ivzb/github_browser/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ivzb/github_browser/ui/login/LoginViewModel.kt
@@ -3,4 +3,9 @@ package com.ivzb.github_browser.ui.login
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor() : ViewModel()
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+    fun onLogin() {
+
+    }
+}
