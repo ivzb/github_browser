1454102c47e3b5f4f8393e2c182c0c1141a3531a
ivzb <izahariev96@gmail.com>
2020-05-08 19:27:19 +0300

    open github authorization

--- app/src/main/java/com/ivzb/github_browser/ui/login/LoginFragment.kt
+++ app/src/main/java/com/ivzb/github_browser/ui/login/LoginFragment.kt
@@ -1,11 +1,17 @@
 package com.ivzb.github_browser.ui.login
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.ivzb.github_browser.BuildConfig
 import com.ivzb.github_browser.databinding.FragmentLoginBinding
+import com.ivzb.github_browser.domain.EventObserver
 import com.ivzb.github_browser.util.provideViewModel
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -28,6 +34,31 @@ class LoginFragment : DaggerFragment() {
             lifecycleOwner = viewLifecycleOwner
         }
 
+        loginViewModel.loginClick.observe(viewLifecycleOwner, EventObserver {
+            openGithubAuthorization()
+        })
+
         return binding.root
+    }
+
+    private fun openGithubAuthorization() {
+        val uri = Uri.Builder().run {
+            scheme(BuildConfig.authorization_scheme)
+            path(BuildConfig.authorization_path)
+            appendQueryParameter(CLIENT_ID, BuildConfig.client_id)
+            appendQueryParameter(REDIRECT_URI, BuildConfig.redirect_uri)
+            appendQueryParameter(SCOPE, REPO)
+            build()
+        }
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    companion object {
+
+        const val CLIENT_ID = "client_id"
+        const val REDIRECT_URI = "redirect_uri"
+        const val SCOPE = "scope"
+        const val REPO = "repo"
     }
 }
--- app/src/main/java/com/ivzb/github_browser/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ivzb/github_browser/ui/login/LoginViewModel.kt
@@ -1,11 +1,15 @@
 package com.ivzb.github_browser.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ivzb.github_browser.domain.Event
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor() : ViewModel() {
 
-    fun onLogin() {
+    val loginClick = MutableLiveData<Event<Boolean>>()
 
+    fun onLogin() {
+        loginClick.postValue(Event(true))
     }
 }
